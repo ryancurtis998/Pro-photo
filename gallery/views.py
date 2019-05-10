e6e8fe0e8c23261fb8e5713b00d23a5ecd6323cb
santa <ryancurtis998@yahoo.com>
2019-05-10 12:36:02 +0300

    updated views.py

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def start(request):
+    
+    index_images = Image.objects.all()
+    return render(request,'index.html', {'index_images':index_images})
+
+def search_results(request):
+
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_image = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'index.html',{"message":message,"index_images": searched_image})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'index.html',{"message":message})
