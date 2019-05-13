0db209252c15201a7b2beabfe6ad370f46d525a7
santa <ryancurtis998@yahoo.com>
2019-05-13 12:08:20 +0300

    ammend

--- gallery/views.py
+++ gallery/views.py
@@ -17,8 +17,8 @@ def search_results(request):
         searched_image = Image.search_by_category(search_term)
         message = f"{search_term}"
 
-        return render(request, 'index.html',{"message":message,"index_images": searched_image})
+        return render(request, 'search.html',{"message":message,"index_images": searched_image})
 
     else:
         message = "You haven't searched for any term"
-        return render(request, 'index.html',{"message":message})
+        return render(request, 'search.html',{"message":message})
